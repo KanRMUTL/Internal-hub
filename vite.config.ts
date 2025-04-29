4d2a0a9ab2775236b2dd0c505ad47f3dc59d8c45
Kan <kanlowongsa20@gmail.com>
2025-04-29 14:23:41 +0700

    config alias path

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      src: '/src',
+      app: '/src/app',
+      pages: '/src/pages',
+      features: '/src/features',
+      shared: '/src/shared',
+    },
+  },
 })
