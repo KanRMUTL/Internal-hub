855e8c8b8b551231083f044e3940386b49815b5d
Kan <kanlowongsa20@gmail.com>
2025-05-01 00:25:53 +0700

    fixed build failed

--- src/pages/Home/ui/Home.tsx
+++ src/pages/Home/ui/Home.tsx
@@ -5,10 +5,22 @@ import { Box, Button } from 'shared/ui'
 const Home = () => {
   return (
     <Box $flex $justify="center" $align="center" $gap="xl" $p="lg">
-      <Room id={1} title="Title" description="This is some description." onAdd={(id) => {}} onRemove={(id) => {}} />
+      <Room
+        id={1}
+        title="Title"
+        description="This is some description."
+        onAdd={(id) => {
+          console.log(id)
+        }}
+        onRemove={(id) => {
+          console.log(id)
+        }}
+      />
       <Box>
         <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 1 }}>
-          <Button $variant="info">+ new room</Button>
+          <Button $variant="info" onClick={() => {}}>
+            + new room
+          </Button>
         </motion.div>
       </Box>
     </Box>
