28cc3677beaeae42bf7771d3713e7daa23a8784b
Kan <kanlowongsa20@gmail.com>
2025-05-02 21:04:40 +0700

    created room modal

---
+++ src/features/room-management/ui/RoomModal.tsx
@@ -0,0 +1,69 @@
+// src/features/room-management/ui/RoomModal.tsx
+import { useForm, SubmitHandler } from 'react-hook-form'
+import { Box, Button, Input, Modal, Typography } from 'shared/ui'
+import TextArea from 'shared/ui/TextArea'
+
+export interface RoomForm {
+  name: string
+  description: string
+}
+
+interface RoomModalProps {
+  isOpen: boolean
+  onClose: () => void
+  onSubmit: (formData: RoomForm) => void
+  defaultValues?: RoomForm
+  title?: string
+}
+
+const RoomModal = ({ isOpen, onClose, onSubmit, defaultValues, title = 'New Room' }: RoomModalProps) => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<RoomForm>({ defaultValues })
+
+  const handleClose = () => {
+    onClose()
+    reset()
+  }
+
+  const handleFormSubmit: SubmitHandler<RoomForm> = (data) => {
+    onSubmit(data)
+    reset()
+    onClose()
+  }
+
+  return (
+    <Modal isOpen={isOpen} onClose={handleClose}>
+      <Box $flex $direction="column" $justify="center" $align="center" $p="sm" $gap="sm">
+        <Typography $size="xl" $weight="bold" $color="primary">
+          {title}
+        </Typography>
+        <form onSubmit={handleSubmit(handleFormSubmit)} style={{ width: '100%' }}>
+          <Box $flex $direction="column" $justify="center" $align="center" $gap="md">
+            <Input
+              placeholder="Enter room name..."
+              error={errors.name?.message}
+              {...register('name', {
+                required: { value: true, message: "Don't forget to enter name" },
+                maxLength: { value: 20, message: 'Name must not exceed 20 characters' },
+              })}
+            />
+            <TextArea
+              placeholder="Enter room description..."
+              error={errors.description?.message}
+              {...register('description', {
+                maxLength: { value: 30, message: 'Description must not exceed 30 characters' },
+              })}
+            />
+            <Button type="submit">Save</Button>
+          </Box>
+        </form>
+      </Box>
+    </Modal>
+  )
+}
+
+export default RoomModal
