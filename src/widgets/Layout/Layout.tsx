aea1b127f084d646e444a9aaa8c5e1dd859bc00a
Kan <kanlowongsa20@gmail.com>
2025-05-13 22:39:34 +0700

    updated flow in navbar

--- src/widgets/Layout/Layout.tsx
+++ src/widgets/Layout/Layout.tsx
@@ -1,17 +1,27 @@
 import { Wrapper, Nav, Main } from './styled'
 import ToggleThemeButton, { useTheme } from 'features/toggleTheme'
 import { Outlet } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+
 interface LayoutProps {
   logoPath: string
 }
 
 const Layout = ({ logoPath }: LayoutProps) => {
   const { mode } = useTheme()
+  const navigate = useNavigate()
+
   return (
     <Wrapper>
       <Nav $light={mode == 'LIGHT'}>
         <div>
-          <img src={logoPath} />
+          <img
+            src={logoPath}
+            style={{ cursor: 'pointer' }}
+            onClick={() => {
+              navigate('/')
+            }}
+          />
         </div>
         <div>
           <ToggleThemeButton />
