131ef859f68996f0a0a171f4675c2af415a74ff3
Kan <kanlowongsa20@gmail.com>
2025-05-18 17:06:56 +0700

    created table ui

--- src/shared/ui/Table/Table.tsx
+++ src/shared/ui/Table/Table.tsx
@@ -1,4 +1,7 @@
-export interface TableColumn<T> {
+interface BaseData {
+  id: string
+}
+export interface TableColumn<T> extends BaseData {
   key: keyof T
   label: string
   render?: (value: T[keyof T], row: T) => React.ReactNode
@@ -6,29 +9,32 @@ export interface TableColumn<T> {
   width?: string
 }
 
-export interface DynamicTableProps<T> {
+export interface DynamicTableProps<T extends BaseData> {
   columns: TableColumn<T>[]
   data: T[]
   rowkey: keyof T
 }
 
-const Table = <T,>({ columns, data, rowkey }: DynamicTableProps<T>) => {
+const Table = <T extends BaseData>({ columns, data }: DynamicTableProps<T>) => {
   return (
     <table>
       <thead>
         <tr>
-          {columns.map((col) => (
-            <th key={String(col.key)} style={{ textAlign: col.align || 'left', width: col.width, padding: 16 }}>
+          {columns.map((col, i) => (
+            <th
+              key={`${String(col.id)}-${i}`}
+              style={{ textAlign: col.align || 'left', width: col.width, padding: 16 }}
+            >
               {col.label}
             </th>
           ))}
         </tr>
       </thead>
       <tbody>
-        {data.map((row, idx) => (
-          <tr key={String(row[rowkey]) || idx}>
-            {columns.map((col) => (
-              <td key={String(col.key)} style={{ textAlign: col.align || 'left', padding: '12px 24px' }}>
+        {data.map((row, i) => (
+          <tr key={`${String(row.id)}-${i}`}>
+            {columns.map((col, j) => (
+              <td key={`${String(col.id)}-${j}`} style={{ textAlign: col.align || 'left', padding: '12px 24px' }}>
                 {col.render ? col.render(row[col.key], row) : String(row[col.key])}
               </td>
             ))}
--- src/shared/ui/Table/index.ts
+++ src/shared/ui/Table/index.ts
@@ -1,1 +1,1 @@
-export { default } from './Table'
+export { default, type TableColumn } from './Table'
