a71ddc45db9968c237e027ba77b47bf70d49ffed
Kan <kanlowongsa20@gmail.com>
2025-05-18 16:11:44 +0700

    improve toggle

--- src/shared/ui/Toggle/Toggle.tsx
+++ src/shared/ui/Toggle/Toggle.tsx
@@ -1,24 +1,47 @@
 import * as motion from 'motion/react-client'
 
+interface ToggleColor {
+  handle: string
+  background: string
+  border: string
+}
 interface ToggleProps {
   isOn: boolean
+  onStyle?: ToggleColor
+  offStyle?: ToggleColor
   onToggleSwitch: VoidFunction
 }
 
-const Toggle = ({ isOn, onToggleSwitch }: ToggleProps) => {
+const Toggle = ({
+  isOn,
+  offStyle = {
+    handle: '#1E9CED',
+    background: '#0a0a0a',
+    border: '#0a0a0a',
+  },
+  onStyle = {
+    handle: '#F3C623',
+    background: '#3C3D37',
+    border: '#F3C623',
+  },
+  onToggleSwitch,
+}: ToggleProps) => {
   return (
     <div
       className="toggle-container"
       style={{
         ...container,
-        backgroundColor: isOn ? '#0a0a0a' : '#1E9CED',
-        justifyContent: 'flex-' + (isOn ? 'start' : 'end'),
+        backgroundColor: isOn ? onStyle.background : offStyle.background,
+        justifyContent: 'flex-' + (isOn ? 'end' : 'start'),
+        borderWidth: 2,
+        borderStyle: 'solid',
+        borderColor: isOn ? onStyle.border : offStyle.border,
       }}
       onClick={onToggleSwitch}
     >
       <motion.div
         className="toggle-handle"
-        style={{ ...handle, backgroundColor: isOn ? '#1E9CED' : '#0a0a0a' }}
+        style={{ ...handle, backgroundColor: isOn ? onStyle.handle : offStyle.handle }}
         layout
         transition={{
           type: 'spring',
