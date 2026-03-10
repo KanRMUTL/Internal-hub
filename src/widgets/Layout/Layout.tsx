cfbc38365ef91a800bcdd833273c6024b74aaa3a
Kan <kanlowongsa20@gmail.com>
2026-03-10 16:56:49 +0000

    refactor: Batch 2 Layout widget to Tailwind CSS

--- src/widgets/Layout/Layout.tsx
+++ src/widgets/Layout/Layout.tsx
@@ -1,53 +1,52 @@
-import { Wrapper, Nav, Main } from './styled'
 import { ToggleThemeButton, useTheme } from 'features/toggle-theme'
-import { Outlet } from 'react-router-dom'
-import { useNavigate } from 'react-router-dom'
+import { Outlet, useNavigate } from 'react-router-dom'
 import { masterLogo, hatohub } from 'shared/assets'
-import { Box, Breadcrumbs, Container } from 'shared/ui'
-import styled from 'styled-components'
+import { Breadcrumbs, Container } from 'shared/ui'
+import { cva } from 'class-variance-authority'
+
+const navVariants = cva('flex items-center justify-between p-2 shadow-md', {
+  variants: {
+    light: {
+      true: 'bg-blue-700',
+      false: 'bg-gray-900',
+    },
+  },
+  defaultVariants: {
+    light: true,
+  },
+})
+
+const logoWrapperVariants = cva('flex items-center gap-2 rounded-md bg-white pb-1 pl-2 pr-4 pt-1')
 
 const Layout = () => {
   const { mode } = useTheme()
   const navigate = useNavigate()
 
   return (
-    <Wrapper>
-      <Nav $light={mode == 'LIGHT'}>
-        <LogoWrapper $display="flex" $align="center" $gap="sm" $pl="sm" $pr="md" $pt="xs" $pb="xs">
+    <div className="flex flex-col p-0">
+      <nav className={navVariants({ light: mode === 'LIGHT' })}>
+        <div className={logoWrapperVariants()}>
           <img
             src={masterLogo}
             height={42}
-            style={{ cursor: 'pointer' }}
-            onClick={() => {
-              navigate('/')
-            }}
+            className="cursor-pointer"
+            onClick={() => navigate('/')}
+            alt="Master Logo"
           />
-          <img
-            src={hatohub}
-            height={22}
-            style={{ cursor: 'pointer' }}
-            onClick={() => {
-              navigate('/')
-            }}
-          />
-        </LogoWrapper>
+          <img src={hatohub} height={22} className="cursor-pointer" onClick={() => navigate('/')} alt="Hatohub Logo" />
+        </div>
         <div>
           <ToggleThemeButton />
         </div>
-      </Nav>
-      <Main>
+      </nav>
+      <main className="w-full">
         <Container>
           <Breadcrumbs />
           <Outlet />
         </Container>
-      </Main>
-    </Wrapper>
+      </main>
+    </div>
   )
 }
 
 export default Layout
-
-const LogoWrapper = styled(Box)`
-  background-color: ${({ theme }) => theme.colors.white};
-  border-radius: ${({ theme }) => theme.borderRadius.md};
-`
